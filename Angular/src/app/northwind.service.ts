b93ee84d2f42a1f9de0d49e904357c4d15aabc05
btasma <git@bentasma.nl>
2020-02-16 20:09:31 +0100

    Updated url frontend

--- Angular/src/app/northwind.service.ts
+++ Angular/src/app/northwind.service.ts
@@ -13,7 +13,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
-    private BASE_URL: string = 'https://localhost:44393/Products';
+    private BASE_URL: string = 'https://localhost:44393/Blogs/DSR';
 
     constructor(private http: HttpClient) { }
 
